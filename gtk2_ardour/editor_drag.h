84d5ba2da6be6d29b8b8cd6f627ec285e2dfb8ec
Paul Davis <paul@linuxaudiosystems.com>
2020-11-19 09:03:28 -0700

    changes to get program to actually run (missing symbols)
    Still crashes at startup due to static initialization of initial tempo map.

--- gtk2_ardour/editor_drag.h
+++ gtk2_ardour/editor_drag.h
@@ -915,6 +915,8 @@ private:
 	bool     _drag_valid;
 };
 
+#warning NUTEMPO may or may not need this in the new world
+#if 0
 /** tempo curve twist drag */
 class TempoTwistDrag : public Drag
 {
@@ -944,7 +946,7 @@ private:
 	bool _drag_valid;
 	XMLNode* _before_state;
 };
-
+#endif
 
 /** tempo curve twist drag */
 class TempoEndDrag : public Drag
