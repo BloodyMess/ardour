2eed4a50bb31421885e3047dc5650e65b267ba5b
Paul Davis <paul@linuxaudiosystems.com>
2020-11-23 16:45:08 -0700

    continued work on using new tempo map API (before switch to RCU)

--- gtk2_ardour/editor.h
+++ gtk2_ardour/editor.h
@@ -583,8 +583,8 @@ public:
 
 	void mouse_add_new_tempo_event (Temporal::timepos_t where);
 	void mouse_add_new_meter_event (Temporal::timepos_t where);
-	void edit_tempo_section (Temporal::TempoPoint*);
-	void edit_meter_section (Temporal::MeterPoint*);
+	void edit_tempo_section (Temporal::TempoPoint&);
+	void edit_meter_section (Temporal::MeterPoint&);
 
 	void add_region_marker ();
 	void clear_region_markers ();
