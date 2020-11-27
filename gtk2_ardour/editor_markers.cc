951ceedcf9d43fd6e62ce91d3f94b1918341aa14
Paul Davis <paul@linuxaudiosystems.com>
2020-11-27 15:44:34 -0700

    fix up memento command use for tempo map undo/redo

--- gtk2_ardour/editor_markers.cc
+++ gtk2_ardour/editor_markers.cc
@@ -1489,8 +1489,7 @@ Editor::toggle_tempo_type ()
 		tmap->set_ramped (tempo, !tempo.ramped());
 
 		XMLNode &after = tmap->get_state();
-#warning NUTEMPO map object may change
-		//_session->add_command(new MementoCommand<Temporal::TempoMap>(_session->tempo_map(), &before, &after));
+		_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 		commit_reversible_command ();
 	}
 }
@@ -1512,8 +1511,7 @@ Editor::toggle_tempo_clamped ()
 		if (tempo.set_clamped (!tempo.clamped())) {
 
 			XMLNode &after = tmap->get_state();
-#warning NUTEMPO paul knows the drill by now
-			//_session->add_command(new MementoCommand<Temporal::TempoMap>(_session->tempo_map(), &before, &after));
+			_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 			commit_reversible_command ();
 		} else {
 			abort_reversible_command ();
@@ -1538,8 +1536,7 @@ Editor::ramp_to_next_tempo ()
 		Temporal::TempoPoint & tempo (tm->tempo());
 		if (tmap->set_ramped (tempo, !tempo.ramped())) {
 			XMLNode &after = tmap->get_state();
-#warning NUTEMPO see previous warning
-			// _session->add_command(new MementoCommand<Temporal::TempoMap>(_session->tempo_map(), &before, &after));
+			_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 			commit_reversible_command ();
 		} else {
 			abort_reversible_command ();
