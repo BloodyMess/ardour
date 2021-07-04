1620dc15ad3384618132a9946667c5a5becc8d51
Paul Davis <paul@linuxaudiosystems.com>
2021-07-04 09:35:42 -0600

    fix declaration of {Public,}Editor::do_import() (post rebase conflict resolution)

--- gtk2_ardour/public_editor.h
+++ gtk2_ardour/public_editor.h
@@ -271,7 +271,8 @@ public:
 	/** Import existing media */
 	virtual void do_import (std::vector<std::string> paths, Editing::ImportDisposition, Editing::ImportMode mode, ARDOUR::SrcQuality,
 	                        ARDOUR::MidiTrackNameSource, ARDOUR::MidiTempoMapDisposition, Temporal::timepos_t&,
-	                        boost::shared_ptr<ARDOUR::PluginInfo> instrument=boost::shared_ptr<ARDOUR::PluginInfo>()) = 0;
+	                        boost::shared_ptr<ARDOUR::PluginInfo> instrument=boost::shared_ptr<ARDOUR::PluginInfo>(),
+	                        bool with_markers = false) = 0;
 	virtual void do_embed (std::vector<std::string> paths, Editing::ImportDisposition, Editing::ImportMode mode,
 	                       Temporal::timepos_t&,
 	                       boost::shared_ptr<ARDOUR::PluginInfo> instrument=boost::shared_ptr<ARDOUR::PluginInfo>()) = 0;
